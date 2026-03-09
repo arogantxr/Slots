442397abfc7c09b5d5a6ebe1d79e8cd5ece8b934
Luca Kohberger <lkohberg@htl-steyr.ac.at>
2026-03-09 14:41:15 +0100

    add Slots_Client class for server connection and update Homescreen_Controller to handle client connections

--- src/main/java/htl/steyr/slots/gameLogik/Slots_Client.java
+++ src/main/java/htl/steyr/slots/gameLogik/Slots_Client.java
@@ -1,4 +1,40 @@
 package htl.steyr.slots.gameLogik;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Slots_Client {
+    private String playerName;
+    private String serverIP;
+    private int serverPort;
+    private Socket socket;
+
+    public Slots_Client(String playerName, String serverIP, int serverPort) {
+        this.playerName = playerName;
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
+    }
+
+    public void connect() throws IOException {
+        // Verbindung zum Server aufbauen
+        socket = new Socket(serverIP, serverPort);
+        System.out.println("Verbunden mit Server: " + serverIP + ":" + serverPort);
+        System.out.println("Spielername: " + playerName);
+
+        // Hier die weitere Verbindungslogik implementieren
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void disconnect() {
+        try {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
