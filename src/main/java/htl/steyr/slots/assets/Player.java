09c66de215e1f08df5ddd3b77dcc509b9a9aebaf
fabi <fabi.obermayr16@gmail.com>
2026-03-11 13:48:45 +0100

    updated Player

--- src/main/java/htl/steyr/slots/assets/Player.java
+++ src/main/java/htl/steyr/slots/assets/Player.java
@@ -1,17 +1,20 @@
 package htl.steyr.slots.assets;
+
 import java.util.List;
 
 public class Player {
 
     private final String name;
-    private int lives;
     private final Slotmachine slotmachine;
 
+    private boolean alive = true;
+    private boolean usedRespin = false;
+    private boolean submitted = false;
+    private int claimedHearts = 0;
     private List<String> lastSpin = List.of();
 
-    public Player(String name, int lives, Slotmachine slotmachine) {
+    public Player(String name, Slotmachine slotmachine) {
         this.name = name;
-        this.lives = lives;
         this.slotmachine = slotmachine;
     }
 
@@ -20,23 +23,58 @@ public class Player {
         return lastSpin;
     }
 
-    public List<String> getLastSpin() {
+    public List<String> deadSpin() {
+        lastSpin = slotmachine.spin(1);
         return lastSpin;
     }
 
-    public void loseLife() {
-        if (lives > 0) lives--;
+    public int countHearts() {
+        return slotmachine.countHearts(lastSpin);
     }
 
-    public boolean isAlive() {
-        return lives > 0;
+    public void resetForRound() {
+        submitted = false;
+        claimedHearts = 0;
+        lastSpin = List.of();
     }
 
-    public int getLives() {
-        return lives;
+    public void useRespin() {
+        usedRespin = true;
+    }
+
+    public void eliminate() {
+        alive = false;
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean hasUsedRespin() {
+        return usedRespin;
+    }
+
+    public boolean hasSubmitted() {
+        return submitted;
+    }
+
+    public void setSubmitted(boolean submitted) {
+        this.submitted = submitted;
+    }
+
+    public int getClaimedHearts() {
+        return claimedHearts;
+    }
+
+    public void setClaimedHearts(int claimedHearts) {
+        this.claimedHearts = claimedHearts;
+    }
+
+    public List<String> getLastSpin() {
+        return lastSpin;
     }
 }
