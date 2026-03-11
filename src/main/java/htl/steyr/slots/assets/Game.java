17d40873a05fd5512f8026d3eddf6691f7fd684b
fabi <fabi.obermayr16@gmail.com>
2026-03-11 12:23:27 +0100

    added basic methods to Game class

--- src/main/java/htl/steyr/slots/assets/Game.java
+++ src/main/java/htl/steyr/slots/assets/Game.java
@@ -1,8 +1,38 @@
 package htl.steyr.slots.assets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-    public Game() {
+    private final List<Player> players = new ArrayList<>();
+    private int currentPlayerIndex;
+    private Claim currentClaim;
 
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void startRound() {
+        for (Player player : players) {
+            if (player.isAlive()) {
+                player.spin();
+            }
+        }
+        currentClaim = null;
+        currentPlayerIndex = 0;
+    }
+
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Claim getCurrentClaim() {
+        return currentClaim;
+    }
+
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayerIndex);
     }
 }
